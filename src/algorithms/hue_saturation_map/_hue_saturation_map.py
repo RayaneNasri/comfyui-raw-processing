def7b9bc5896ea32a5a3206b36c783f0444bddab
Amayas HADDAG <amayas.haddag@telecom-paris.fr>
2026-02-27 10:01:42 +0100

    feature/hue-sat-map: Added mired values conversion from calibration illuminants

--- src/algorithms/hue_saturation_map/_hue_saturation_map.py
+++ src/algorithms/hue_saturation_map/_hue_saturation_map.py
@@ -1,6 +1,23 @@
 from torch import Tensor 
 
-def apply_hue_saturation_map(rgb_image: Tensor, wb_gains: Tensor, low_temp_lut: Tensor, high_temp_lut: Tensor) -> Tensor: 
+def _mired_value_from_calibration_illuminant(calib_illum: int) -> float:
+    factor = 1_000_00
+    illuminant_to_kelvin = {
+        1: 5500,  # Daylight
+        3: 3200,  # Tungsten
+        17: 2856, # Standard Illuminant A
+        20: 5503, # D55
+        21: 6504, # D65
+        23: 5003  # D50
+    }
+    
+    temperature = illuminant_to_kelvin.get(calib_illum)
+    if temperature is None: 
+        raise Exception # TODO : implement more formal exception errors raising (define isp_exceptions.py file and define them)
+    
+    return factor / temperature
+
+def apply_hue_saturation_map(rgb_image: Tensor, wb_gains: Tensor, low_temp_lut: Tensor, high_temp_lut: Tensor, color_matrix: Tensor) -> Tensor: 
     """
     Applies the camera profile `HueSatMap` LUTs depending on white balance gains.
     

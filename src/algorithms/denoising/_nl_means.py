57a37cdc69e6863e6ef3a1a25b6abbaac2451711
Rayane Nasri <rayane.nasri@telecom-paris.fr>
2026-03-05 17:09:06 +0100

    feature/noise-reduction : Implement a decorator that validates the input image tensor

--- src/algorithms/denoising/_nl_means.py
+++ src/algorithms/denoising/_nl_means.py
@@ -2,12 +2,16 @@ import torch
 import numpy as np
 import cv2 
 
+# Decorators
+from algorithms._utils import validate_image_input
+
+@validate_image_input
 def nl_means(img: torch.Tensor,
              h: float = 3,
              hColor: float = 3,
              templateWindowSize: int = 7,
              searchWindowSize: int = 21
-        ):
+        )-> torch.Tensor:
     """
     Simple wrapper for open-cv function fastNlMeansDenoisingColored
 
@@ -27,24 +31,8 @@ def nl_means(img: torch.Tensor,
     Returns
     -------
     img_wb : torch.Tensor
-        White balanced image
-    """
-    # -- Global tests
-    if not isinstance(img, torch.Tensor) :
-        raise TypeError("img must be a torch Tensor")
-    if (img.ndim != 3) :
-        raise ValueError("img must be 3 dim tensor : H, W, C")
-  
-    img_h, img_w, img_c = img.shape
-    if img_h * img_w == 0 :
-        raise ValueError("img is empty")
-    if img_c != 3 :
-        raise ValueError(f"img shape must be (H, W, 3), but found (H, W, {img_c})")
-    
-    if not (0. <= img).all() and (img <= 1.).all() :
-        raise ValueError("all img entries must be between 0 and 1")
-    
-    # -- Specific tests
+        Denoised image
+    """   
     if not isinstance(h, (int, float)):
         raise TypeError("h must be a number (float or int)")
     if not isinstance(hColor, (int, float)):
@@ -54,6 +42,7 @@ def nl_means(img: torch.Tensor,
     if hColor < 0. :
         raise ValueError("hColor must be positive")
     
+    img_h, img_w, img_c = img.shape
     if not isinstance(searchWindowSize, int) :
         raise TypeError("searchWindowSize must be an integer")
     if searchWindowSize < 1 :
@@ -74,7 +63,7 @@ def nl_means(img: torch.Tensor,
         raise ValueError("searchWindowSize cannot be strictly smaller than templateWindowSize")
 
 
-    # Wrapper start
+    # Wrapper start -- 
     src = torch.clip(img * 255, min=0, max=255)
 
     # This line:

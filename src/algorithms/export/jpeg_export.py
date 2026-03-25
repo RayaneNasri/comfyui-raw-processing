f382c80fe1c6c17b45b781ef44e0431ed4291f5e
Amayas HADDAG <amayas.haddag@telecom-paris.fr>
2026-03-26 00:48:25 +0100

    feature/image-compression: Ruff formatting to pass CI/CD after MR

--- src/algorithms/export/jpeg_export.py
+++ src/algorithms/export/jpeg_export.py
@@ -1,10 +1,11 @@
 import torch
 
-from torch import Tensor 
+from torch import Tensor
 from torchvision.io import write_jpeg
 
-def export_jpeg(image: Tensor, path: str, quality: int = 75): 
-    """ 
+
+def export_jpeg(image: Tensor, path: str, quality: int = 75):
+    """
     Exports an sRGB tensor image to a JPEG file.
     Args:
         image (Tensor): A tensor of shape [H, W, 3] with float values in the range [0, 1].
@@ -13,7 +14,6 @@ def export_jpeg(image: Tensor, path: str, quality: int = 75):
     """
 
     img_reshaped = image.permute(2, 0, 1)
-    img_def = torch.nan_to_num(img_reshaped, nan = 0.0, posinf = 1.0, neginf = 0.0)
+    img_def = torch.nan_to_num(img_reshaped, nan=0.0, posinf=1.0, neginf=0.0)
     image_uint8 = (img_def * 255).clamp(0, 255).byte()
-    write_jpeg(image_uint8, path, quality = quality)
-
+    write_jpeg(image_uint8, path, quality=quality)
--- src/custom_nodes/export/jpeg_export_node.py
+++ src/custom_nodes/export/jpeg_export_node.py
@@ -3,6 +3,7 @@ import os
 
 from algorithms.export.jpeg_export import export_jpeg
 
+
 class JpegExportNode:
     @classmethod
     def INPUT_TYPES(cls):
@@ -23,22 +24,23 @@ class JpegExportNode:
     def process(self, image, folder_path, filename, quality):
         if not os.path.exists(folder_path):
             raise ValueError(f"The folder path '{folder_path}' does not exist.")
-        
+
         if os.path.isfile(folder_path):
-            raise ValueError(f"The folder path '{folder_path}' is a file, not a directory.")
-        
+            raise ValueError(
+                f"The folder path '{folder_path}' is a file, not a directory."
+            )
+
         if os.path.exists(f"{folder_path}/{filename}.jpg"):
-            raise ValueError(f"The file '{filename}.jpg' already exists in the folder '{folder_path}'. Please choose a different filename or folder path.")
-        
+            raise ValueError(
+                f"The file '{filename}.jpg' already exists in the folder '{folder_path}'. Please choose a different filename or folder path."
+            )
+
         squeezed_image = image.squeeze(0)  # Remove batch dimension if it exists
         export_jpeg(squeezed_image, f"{folder_path}/{filename}.jpg", quality)
 
         return ()
 
-NODE_CLASS_MAPPINGS = {
-    "JpegExportNode": JpegExportNode
-}
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "JpegExportNode": "Save JPEG (Custom Path)"
-}
+NODE_CLASS_MAPPINGS = {"JpegExportNode": JpegExportNode}
+
+NODE_DISPLAY_NAME_MAPPINGS = {"JpegExportNode": "Save JPEG (Custom Path)"}
